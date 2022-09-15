3f85e09aa500ec4a17b4199ab9a2e3e7fe80d141
Al3imran <111256161+Al3imran@users.noreply.github.com>
2022-09-15 20:59:27 +0300

    1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+      if (i !=0)
+      {
+        i = 98;
+        positive_or_negative(i);
+      }
+  else
+  { putchar(%d 0 is zero \n)
+        return (0);
+}
